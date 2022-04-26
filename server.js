e6c752f6c8d3cdb9d1c7d60cdf66fc0109b24b63
Weston <barnesweston@gmail.com>
2022-04-25 21:59:02 -0500

    create new note function

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { fstat } = require('fs');
 const app = express();
 const path = require('path');
 
@@ -9,9 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.send('hello world');
-});
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note)
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray } )
+    );
+    return note;
+};
+
 
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -23,14 +31,23 @@ app.get('/api/notes', (req,res) => {
 
 app.post('/api/notes', (req,res) => {
     // receive new note to save, add to db.json, retun new note to client
+    req.body.id = note.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    
+    res.json(note);
 
     // find way to ad dunique id when saved (npm packages???)
 })
 
-app.get('/*', (req,res) => {
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`server live on PORT ${PORT}`);
+    console.log(`API server on PORT ${PORT}`);
 });
